e2cd54b23958d3a86ea1b6b6c726761e67c94440
Troy Enzo <you@example.com>
2023-06-05 14:09:27 +0800

    removed testimonial code root

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import HeroSection from '../components/HeroSection';
 import Image from 'next/image';
 import Features from '../components/Features';
-import Testimonials from '../components/Testimonials';
+// import Testimonials from '../components/Testimonials';
 
 export default function Home() {
   return (
