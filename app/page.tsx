9f5d9653779535807d1434ca013ca08f12f9ff61
Troy Enzo <you@example.com>
2023-06-05 14:14:14 +0800

    add features

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,8 @@ export default function Home() {
   return (
     <>
       <HeroSection />
-      {/* <Features />
-      <Testimonials /> */}
+      <Features />
+      {/* <Testimonials /> */}
     </>
   );
 }
